3dcdaad4a2c217c5603054db7cc9f7c10ce9f95e
Omer Chen <omerchen15@gmail.com>
2020-04-23 16:04:50 +0300

    complete header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/images/logo.svg";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" dir="rtl">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>כתבו מכתב ליקירכם, המכתבים נשלחים לשום מקום.</p>
+        <p>השתמשו באתר בשביל לפרוק ולשתף את יקיריכם בכל העולה על רוחכם.</p>
       </header>
     </div>
   );
